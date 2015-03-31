4713fcf29e08e940a5edd10f6afb33cf4fa74056
Craig Barstow <craig.r.barstow@gmail.com>
2015-03-31 13:42:54 -0400

    resolve Christina's issues

--- spec/factories/tutorials.rb
+++ spec/factories/tutorials.rb
@@ -1,7 +1,7 @@
 FactoryGirl.define do
   factory :tutorial do
     sequence(:title) {|n| "Tutorial#{n}"}
-    sequence(:url) {|n| "http://www.#{n}.com"}
+    sequence(:url) {|n| "http://www.an-tutorial#{n}.com"}
     language 'Ruby'
   end
 end
