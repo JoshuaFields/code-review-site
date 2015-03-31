1c6f4fc4c8f3e2a7a5b93c89183dadadb8be4499
Ray Hamel <rayghamel@gmail.com>
2015-03-31 15:20:05 -0400

    addressed Christina's comments, hopefully fixed tab/spaces issue, deleted make-it-so detritus

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,8 +1,8 @@
 class ReviewsController < ApplicationController
   def create
     @tutorial = Tutorial.find(params[:tutorial_id])
-    @review = @tutorial.reviews.create(review_params)
-    if @review.valid?
+    @review = @tutorial.reviews.new(review_params)
+    if @review.save
       redirect_to tutorial_path(@tutorial)
     else
       render 'tutorials/show'
