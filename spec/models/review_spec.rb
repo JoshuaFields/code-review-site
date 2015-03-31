e79f33e6be462c520949f38925b6cd0a42178f2b
Ray Hamel <rayghamel@gmail.com>
2015-03-31 14:41:52 -0400

    add unit tests for reviews model

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Review, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to :tutorial }
+
+  it { should have_valid(:body).when("It's amazing!") }
+  it { should_not have_valid(:body).when("") }
+  it { should_not have_valid(:body).when("a") }
+  it { should_not have_valid(:body).when("a" * 10000) }
+
+  it { should have_valid(:rating).when(5) }
+  it { should_not have_valid(:rating).when(50) }
 end
