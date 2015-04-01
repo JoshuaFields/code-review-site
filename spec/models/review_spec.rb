9cb3a8e2f276937b124008a436ade9c1650378d6
Ray Hamel <rayghamel@gmail.com>
2015-04-01 11:07:24 -0400

    add unit tests for user/review relationship

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Review, type: :model do
   it { should belong_to :tutorial }
+  it { should belong_to :user }
 
   it { should have_valid(:body).when("It's amazing!") }
   it { should_not have_valid(:body).when("") }
