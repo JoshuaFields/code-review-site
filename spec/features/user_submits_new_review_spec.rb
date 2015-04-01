0fdb1d5a7014031772fe2dec696f8cea47d02b8d
Ray Hamel <rayghamel@gmail.com>
2015-04-01 12:05:51 -0400

    fixed conflicts, created user/tutorial relationship/validations, updated acceptance tests and factories

--- spec/features/user_submits_new_review_spec.rb
+++ spec/features/user_submits_new_review_spec.rb
@@ -1,15 +1,17 @@
 require 'rails_helper'
 
 feature %q(
-  As a user
+  As an authenticated user
   I want to submit a new review for a tutorial
   so that others can know my important opinion.
 ) do
   scenario 'Submits a new review' do
-    testtutorial = FactoryGirl.create(:tutorial)
-    visit tutorial_path(testtutorial.id)
+    test_tutorial = FactoryGirl.create(:tutorial)
+    sign_in test_tutorial.user
+    visit tutorial_path(test_tutorial.id)
     select '4', from: 'Rating'
     fill_in 'Body', with: 'This is a totally awesome tutorial!'
     expect(page).to have_content('This is a totally awesome tutorial!')
+    expect(page).to have_content('4')
   end
 end
