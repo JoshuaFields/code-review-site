8da44a2fcd07c186977ff9eab6e17c7ac12f677b
Ray Hamel <rayghamel@gmail.com>
2015-04-01 10:54:49 -0400

    Add has_many :reviews relationship to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+  # Should NOT destroy dependent reviews when deleted.
+  has_many :reviews
 end
