2e18c89d16bbe5b9336f16623dc038ec587a0214
Josh Fields <jfields85@gmail.com>
2015-04-01 13:21:50 -0400

    refactored with hound's feedback

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   resources :tutorials, only: %i(index show new create)
+
   resources :tutorials, only: %i(index show) do
     resources :reviews, only: %i(index create)
   end
