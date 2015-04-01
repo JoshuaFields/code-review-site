69ba90c83c5cc87fe0e7d9d1442d9b63a8d4caa4
Josh Fields <jfields85@gmail.com>
2015-04-01 16:06:36 -0400

    Added pagination, added voting

---
+++ spec/features/user_views_only_10_tutorials_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+feature %q(
+  As a user
+  I want to view only 10 tutorials on the index page
+  So that I'm not overwhelmed
+  ) do
+  scenario 'user views specific tutorial' do
+    11.times { FactoryGirl.create(:tutorial) }
+
+    visit tutorials_path
+    expect(page).to have_content("Next › Last »")
+  end
+end
