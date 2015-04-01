d8e9323e9555cfa4f2bdfc30004d0074d83e3213
Ray Hamel <rayghamel@gmail.com>
2015-04-01 10:51:31 -0400

    Require authentication to post review, index reviews to user:

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150331173732) do
+ActiveRecord::Schema.define(version: 20150401144831) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,9 +22,11 @@ ActiveRecord::Schema.define(version: 20150331173732) do
     t.integer  "tutorial_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
   end
 
   add_index "reviews", ["tutorial_id"], name: "index_reviews_on_tutorial_id", using: :btree
+  add_index "reviews", ["user_id"], name: "index_reviews_on_user_id", using: :btree
 
   create_table "tutorials", force: :cascade do |t|
     t.string   "title"
@@ -53,4 +55,5 @@ ActiveRecord::Schema.define(version: 20150331173732) do
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
   add_foreign_key "reviews", "tutorials"
+  add_foreign_key "reviews", "users"
 end
