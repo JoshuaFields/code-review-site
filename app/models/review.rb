9a01a7f95a61f1acd33bebcaac4731d9bcf3bfc7
Ray Hamel <rayghamel@gmail.com>
2015-04-02 15:43:46 -0400

    fix review validation

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,7 +3,7 @@ class Review < ActiveRecord::Base
   belongs_to :user
 
   validates :user, presence: true
-  validates :body, length: { in: 7..8191 }, allow_blank: true
+  validates :body, length: { in: 3..8191 }, allow_blank: true
   validates :rating, presence: true, numericality: {
     only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5
   }
