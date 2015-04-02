59d2586c0c1e719eb47e13f6af43a1d642757bb3
Ray Hamel <rayghamel@gmail.com>
2015-04-02 14:01:25 -0400

    Add edit functionality to tutorials

--- spec/features/user_edits_tutorial_spec.rb
+++ spec/features/user_edits_tutorial_spec.rb
@@ -7,39 +7,39 @@ feature %(
 ) do
   context "user is signed in" do
     before(:each) do
-      user = FactoryGirl.create(:user)
+      @test_user = FactoryGirl.create(:user)
 
       visit new_user_session_path
-
-      fill_in "Email", with: user.email
-      fill_in "Password", with: user.password
+      fill_in "Email", with: @test_user.email
+      fill_in "Password", with: @test_user.password
 
       click_button "Log in"
     end
 
     scenario "fields are pre-populated with existing information" do
-      test_tutorial = FactoryGirl.create(:tutorial)
+      test_tutorial = FactoryGirl.create(:tutorial, user: @test_user)
       visit edit_tutorial_path(test_tutorial)
-      expect(page).to have_content(test_tutorial.title)
-      expect(page).to have_content(test_tutorial.url)
-      expect(page).to have_content(test_tutorial.organization)
-      expect(page).to have_content(test_tutorial.cost)
-      expect(page).to have_content(test_tutorial.language)
-      expect(page).to have_content(test_tutorial.description)
+      expect(page).to have_field(:tutorial_title, with: test_tutorial.title)
+      expect(page).to have_field(:tutorial_url, with: test_tutorial.url)
+      expect(page).to have_field(:tutorial_organization,
+        with: test_tutorial.organization)
+      # expect(page).to have_select(:tutorial_cost, selected: test_tutorial.cost)
+      expect(page).to have_field(:tutorial_language, with: test_tutorial.language)
+      expect(page).to have_field(:tutorial_description, with: test_tutorial.description)
     end
 
     scenario "user wants to go back to tutorial index page" do
-      first_tutorial = FactoryGirl.create(:tutorial)
-      second_tutorial = FactoryGirl.create(:tutorial)
+      first_tutorial = FactoryGirl.create(:tutorial, user: @test_user)
+      second_tutorial = FactoryGirl.create(:tutorial, user: @test_user)
       visit edit_tutorial_path(first_tutorial)
-      click_link "CodeReviewSite"
+      click_link "Home"
       expect(page).to have_content(first_tutorial.title)
       expect(page).to have_content(second_tutorial.title)
     end
 
     scenario "user wants to go back to tutorial show page" do
-      first_tutorial = FactoryGirl.create(:tutorial)
-      second_tutorial = FactoryGirl.create(:tutorial)
+      first_tutorial = FactoryGirl.create(:tutorial, user: @test_user)
+      second_tutorial = FactoryGirl.create(:tutorial, user: @test_user)
       visit edit_tutorial_path(first_tutorial)
       click_link "Back"
       expect(page).to have_content(first_tutorial.title)
@@ -47,7 +47,7 @@ feature %(
     end
 
     scenario "user edits tutorial with valid information" do
-      test_tutorial = FactoryGirl.create(:tutorial)
+      test_tutorial = FactoryGirl.create(:tutorial, user: @test_user)
       visit edit_tutorial_path(test_tutorial)
       fill_in :tutorial_title, with: "A Sweet Rails Tutorial"
       fill_in :tutorial_url,
@@ -57,22 +57,22 @@ feature %(
       fill_in :tutorial_organization, with: "Code School"
       select "Free", from: :tutorial_cost
 
-      click_button "Edit Tutorial"
+      click_button "Submit"
 
       expect(page).to have_content("A Sweet Rails Tutorial")
     end
 
     scenario 'user tries to edit tutorial with missing information' do
-      test_tutorial = FactoryGirl.create(:tutorial)
+      test_tutorial = FactoryGirl.create(:tutorial, user: @test_user)
       visit edit_tutorial_path(test_tutorial)
       fill_in :tutorial_title, with: "A Sweet Rails Tutorial"
       fill_in :tutorial_language, with: "Ruby"
-      fill_in :url, with: nil
+      fill_in :tutorial_url, with: nil
       fill_in :tutorial_description, with: "Y'all should really check this out."
       fill_in :tutorial_organization, with: "Code School"
       select "Free", from: :tutorial_cost
 
-      click_button "Edit Tutorial"
+      click_button "Submit"
 
       expect(page).to have_content("Url can't be blank")
     end
@@ -83,7 +83,7 @@ feature %(
       test_tutorial = FactoryGirl.create(:tutorial)
       visit edit_tutorial_path(test_tutorial)
 
-      expect(page).to have_no_content("Title")
+      expect(page).to have_no_content("Organization")
     end
   end
 end
