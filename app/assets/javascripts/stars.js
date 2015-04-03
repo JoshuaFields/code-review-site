20ae9c874a73b11c889f22da4d03ca5378cf84b2
Josh Fields <jfields85@gmail.com>
2015-04-03 15:20:29 -0400

    Fix hound errors

--- app/assets/javascripts/stars.js
+++ app/assets/javascripts/stars.js
@@ -5,7 +5,7 @@ $( document ).ready(function() {
       // Make sure that the value is in 0 - 5 range, multiply to get width
       var size = Math.max(0, (Math.min(5, val))) * 16;
       // Create stars holder
-      var $span = $('<span />').width(size);
+      var $span = $("<span />").width(size);
       // Replace the numerical value with stars
       $(this).html($span);
     });
