1146dcc91f0e1c4b0b900d7e4a74d6c8326dd394
Ray Hamel <rayghamel@gmail.com>
2015-04-03 15:13:25 -0400

    Used Dan's magic skillz to fix the search.

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,6 +3,7 @@ class Review < ActiveRecord::Base
   belongs_to :user
 
   validates :user, presence: true
+  validates :tutorial, presence: true
   validates :body, length: { in: 3..8191 }, allow_blank: true
   validates :rating, presence: true, numericality: {
     only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5
