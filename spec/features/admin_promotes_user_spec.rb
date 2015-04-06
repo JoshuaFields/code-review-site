6e9a5e9984ed545e05244a5bbc75346f1a053177
Craig Barstow <craig.r.barstow@gmail.com>
2015-04-06 15:13:22 -0400

    Tests for deleting and promoting accounts as admin

---
+++ spec/features/admin_promotes_user_spec.rb
@@ -0,0 +1,84 @@
+require "rails_helper"
+
+feature %(
+  As a God
+  I want to be able to invite my fellow Gods to Mt. Olympus
+  So that mortals may tremble at our presence
+) do
+  let(:test_admin) { FactoryGirl.create(:user, admin: true) }
+  let(:test_admin_two) { FactoryGirl.create(:user, admin: true) }
+  let(:test_user) { FactoryGirl.create(:user) }
+
+  scenario "promotes user to admin" do
+    visit new_user_session_path
+    fill_in "Email", with: test_user.email
+    fill_in "Password", with: test_user.password
+
+    click_button "Log in"
+
+    visit users_path
+
+    expect(page).to have_content("This page requires admin privileges!")
+
+    click_link "Sign Out"
+
+    visit new_user_session_path
+    fill_in "Email", with: test_admin.email
+    fill_in "Password", with: test_admin.password
+
+    click_button "Log in"
+
+    visit users_path
+
+    click_button "toggle#{test_user.id}"
+
+    click_link "Sign Out"
+
+    visit new_user_session_path
+    fill_in "Email", with: test_user.email
+    fill_in "Password", with: test_user.password
+
+    click_button "Log in"
+
+    visit users_path
+
+    expect(page).to have_content(test_admin.email)
+  end
+
+  scenario "demotes admin to user" do
+    visit new_user_session_path
+    fill_in "Email", with: test_admin_two.email
+    fill_in "Password", with: test_admin_two.password
+
+    click_button "Log in"
+
+    test_admin.save
+    visit users_path
+
+    expect(page).to have_content(test_admin.email)
+
+    click_link "Sign Out"
+
+    visit new_user_session_path
+    fill_in "Email", with: test_admin.email
+    fill_in "Password", with: test_admin.password
+
+    click_button "Log in"
+
+    visit users_path
+
+    click_button "toggle#{test_admin_two.id}"
+
+    click_link "Sign Out"
+
+    visit new_user_session_path
+    fill_in "Email", with: test_admin_two.email
+    fill_in "Password", with: test_admin_two.password
+
+    click_button "Log in"
+
+    visit users_path
+
+    expect(page).to have_content("This page requires admin privileges!")
+  end
+end
