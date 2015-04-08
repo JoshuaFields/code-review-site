e16c9ae97cfd99b53c78260e85c6ed65e106414b
Ray Hamel <rayghamel@gmail.com>
2015-04-08 17:31:42 -0400

    Replace calls to 'redis.current' with constant 'REDIS'

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,6 +1,6 @@
 if Rails.env.test? || Rails.env.development?
-  Redis.current = Redis.new(host: 'localhost', port: 6379)
+  REDIS = Redis.new(host: 'localhost', port: 6379)
 else
   uri = URI.parse(ENV["REDISCLOUD_URL"])
-  Redis.current = Redis.new(host: uri.host, port: uri.port, password: uri.password)
+  REDIS = Redis.new(host: uri.host, port: uri.port, password: uri.password)
 end
--- lib/voting.rb
+++ lib/voting.rb
@@ -1,17 +1,17 @@
 module Voting
   def send_upvote(review_id, user_id)
-    if Redis.current.zscore("review_votes_#{review_id}", user_id) == 1
-      Redis.current.zadd("review_votes_#{review_id}", 0, user_id)
+    if REDIS.zscore("review_votes_#{review_id}", user_id) == 1
+      REDIS.zadd("review_votes_#{review_id}", 0, user_id)
     else
-      Redis.current.zadd("review_votes_#{review_id}", 1, user_id)
+      REDIS.zadd("review_votes_#{review_id}", 1, user_id)
     end
   end
 
   def send_downvote(review_id, user_id)
-    if Redis.current.zscore("review_votes_#{review_id}", user_id) == -1
-      Redis.current.zadd("review_votes_#{review_id}", 0, user_id)
+    if REDIS.zscore("review_votes_#{review_id}", user_id) == -1
+      REDIS.zadd("review_votes_#{review_id}", 0, user_id)
     else
-      Redis.current.zadd("review_votes_#{review_id}", -1, user_id)
+      REDIS.zadd("review_votes_#{review_id}", -1, user_id)
     end
   end
 end
