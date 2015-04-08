f8632e279fea35236f56ee4b9feeef056b862769
Craig Barstow <craig.r.barstow@gmail.com>
2015-04-08 15:49:25 -0400

    remove flawed ajax test

--- spec/features/user_votes_on_a_review_spec.rb
+++ spec/features/user_votes_on_a_review_spec.rb
@@ -1,41 +1,43 @@
-"Test is not playing well with Ajax, but it definitely works perfectly."
-
 # require 'rails_helper'
-
+#
 # feature %(
 #   As a user
 #   I want to vote on a tutorial's review
 #   So that I can voice my opinion on its usefulness
 # ) do
-
+#
 #   context "user is signed in" do
 #     before(:each) do
 #       user = FactoryGirl.create(:user)
-
+#
 #       visit new_user_session_path
-
+#
 #       fill_in "Email", with: user.email
 #       fill_in "Password", with: user.password
-
+#
 #       click_button "Log in"
 #     end
-
+#
 #     scenario 'User views a tutorial with a review', js: true do
 #       review = FactoryGirl.create(:review)
-
+#
 #       visit tutorial_path(review.tutorial)
-
+#
 #       expect(page).to have_content(review.body)
-#       expect(page).to have_content("upvote 0 downvote")
-
-#       click_link("upvote")
-#       expect(page).to have_content("upvote 1 downvote")
-
-#       click_link("upvote")
-#       expect(page).to have_content("upvote 0 downvote")
-
-#       click_link("downvote")
-#       expect(page).to have_content("upvote -1 downvote")
+#       expect(page).to have_selector("#review-#{review.id}", text: "0")
+#
+#       find('.small-1').click_link 'upvote'
+#       # click_link("upvote")
+#       # page.execute_script("$('.upvote-img').click();")
+#       expect(page).to have_selector("#review-#{review.id}", text: "1")
+#
+#       find('.small-1').click_link 'upvote'
+#       # page.execute_script("$('.upvote-img').click();")
+#       expect(page).to have_selector("#review-#{review.id}", text: "0")
+#
+#       find('.small-1').click_link 'downvote'
+#       # page.execute_script("$('.downvote-img').click();")
+#       expect(page).to have_selector("#review-#{review.id}", text: "-1")
 #     end
 #   end
 # end
