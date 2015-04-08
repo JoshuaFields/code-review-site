ce4729177a8ccf040406512ea0434c67e6943550
Ray Hamel <rayghamel@gmail.com>
2015-04-08 17:27:16 -0400

    Fix jump to top of page on vote, make Redis initializer more conventional

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,5 +1,6 @@
-if ENV["REDISCLOUD_URL"]
-  Redis.current = Redis.new(:url => ENV["REDISCLOUD_URL"])
-else
+if Rails.env.test? || Rails.env.development?
   Redis.current = Redis.new(host: 'localhost', port: 6379)
+else
+  uri = URI.parse(ENV["REDISCLOUD_URL"])
+  Redis.current = Redis.new(host: uri.host, port: uri.port, password: uri.password)
 end
