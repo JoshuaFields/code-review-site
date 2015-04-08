2b09229bcbf2b90a030052c6585167b00e0d05b6
Craig Barstow <craig.r.barstow@gmail.com>
2015-04-08 17:01:53 -0400

    configure Redis to work with Heroku

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,1 +1,5 @@
-Redis.current = Redis.new(host: 'localhost', port: 6379)
+if ENV["REDISCLOUD_URL"]
+  Redis.current = Redis.new(:url => ENV["REDISCLOUD_URL"])
+else
+  Redis.current = Redis.new(host: 'localhost', port: 6379)
+end
