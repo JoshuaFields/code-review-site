e3d293b075311e6dc3b5683b408275639c026090
Ray Hamel <rayghamel@gmail.com>
2015-04-09 12:40:13 -0400

    more debugging

--- app/assets/javascripts/stars.js
+++ app/assets/javascripts/stars.js
@@ -1,13 +1,20 @@
 $( document ).ready(function() {
+  console.log("document.ready runs");
   function populateStars() {
+    console.log("populateStars runs");
     $( ".stars-holder" ).each(function() {
+      console.log("stars holder found");
       var val = parseFloat($(this).html());
+      console.log("number parsed");
       // Make sure that the value is in 0 - 5 range, multiply to get width
       var size = Math.max(0, (Math.min(5, val))) * 16;
+      console.log("width determined");
       // Create stars holder
       var $span = $("<span />").width(size);
+      console.log("stars holder created");
       // Replace the numerical value with stars
       $(this).html($span);
+      console.log("value replaced");
     });
   }
 
