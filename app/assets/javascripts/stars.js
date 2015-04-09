cc995a4911073a05e4b3d0ec5991f2ccb1f2e97b
Ray Hamel <rayghamel@gmail.com>
2015-04-09 12:24:58 -0400

    Better fix Turbolinks?

--- app/assets/javascripts/stars.js
+++ app/assets/javascripts/stars.js
@@ -1,4 +1,4 @@
-// $( document ).ready(function() {
+$( document ).on("page:load", function() {
   function populateStars() {
     $( ".stars-holder" ).each(function() {
       var val = parseFloat($(this).html());
@@ -12,4 +12,4 @@
   }
 
   populateStars();
-// });
+});
--- app/assets/javascripts/voting.js
+++ app/assets/javascripts/voting.js
@@ -1,4 +1,4 @@
-// $(document).ready(function () {
+$( document ).on("page:load", function() {
     $(".upvote-img, .downvote-img").click(function () {
         var path = $(this).attr("path");
         var reviewID = $(this).attr("reviewID");
@@ -7,4 +7,4 @@
         });
         return false;
     });
-// });
+});
