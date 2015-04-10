5d95f39e0002419fa3c30914870190b358e4f50a
Fred Chapman <areinei123@gmail.com>
2015-04-10 10:19:55 -0400

    fredified

---
+++ app/assets/javascripts/normalizeIndexThumbnails.js
@@ -0,0 +1,18 @@
+function normalizeIndexWrapperHeights(wrapperClass) {
+  var largest = 0;
+
+  $(wrapperClass).each(function() {
+    //console.log($(this).height);
+    if ($(this).height() > largest ) {
+      largest = $(this).height();
+    }
+  });
+  console.log("greatest height = " + String(largest));
+  $(wrapperClass).height(largest);
+}
+
+var indexWrapperClass = ".tutorial-thumbnail-wrapper";
+
+if ($(indexWrapperClass).length) {
+  normalizeIndexWrapperHeights(indexWrapperClass);
+}
